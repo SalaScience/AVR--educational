f24d2b6b923c6c9dc1e3954a69dfe467c5fd7fb9
SalaScience <SalaScience@gmail.com>
2022-12-21 18:02:47 +0100

    Added LCD library (HD44780)

---
+++ LCD_HD44780/lcd_lib.c
@@ -0,0 +1,103 @@
+// ***********************************************************************
+// (c)2022 SalaScience
+// LCD library for HD44780 driver
+// ***********************************************************************
+
+#include "lcd_lib.h"
+
+// Sending 4 bits
+static inline void LCD_send_half_byte(uint8_t data)
+{
+ SET_E;
+ if (data & (1 << 0)) LCD_PORT |= (1 << LCD_D4); else LCD_PORT &=~(1 << LCD_D4);
+ if (data & (1 << 1)) LCD_PORT |= (1 << LCD_D5); else LCD_PORT &=~(1 << LCD_D5);
+ if (data & (1 << 2)) LCD_PORT |= (1 << LCD_D6); else LCD_PORT &=~(1 << LCD_D6);
+ if (data & (1 << 3)) LCD_PORT |= (1 << LCD_D7); else LCD_PORT &=~(1 << LCD_D7);
+ CLR_E; 
+}
+
+// Sending 8 bits (byte)
+void LCD_send_whole_byte(uint8_t data)
+{
+ LCD_send_half_byte(data >> 4);
+ _delay_us(120);
+ LCD_send_half_byte(data);
+ _delay_us(120);
+}
+
+// Sending command
+void LCD_send_cmd(uint8_t cmd)
+{
+ CLR_RS;                                            // clear RS line (0)
+ LCD_send_whole_byte(cmd);
+}
+
+// Sending data (characters/symbols)
+void LCD_send_data (uint8_t data)
+{
+ SET_RS;                                            // set RS line (1)
+ LCD_send_whole_byte(data);
+}
+
+// Initialization
+void LCD_init(void)
+{
+ LCD_PORT_DDR |= (1 << LCD_RS);
+ LCD_PORT_DDR |= (1 << LCD_E);
+ LCD_PORT_DDR |= (1 << LCD_D4) | (1 << LCD_D5) | (1 << LCD_D6)
+ | (1 << LCD_D7);
+ CLR_RS;
+ CLR_E;
+ _delay_ms(50);
+ LCD_send_half_byte(0x03);
+ _delay_ms(4.1);
+ LCD_send_half_byte(0x03);
+ _delay_us(100);
+ LCD_send_half_byte(0x03);
+ _delay_us(100);
+ LCD_send_half_byte(0x02);
+ _delay_us(100);
+ // ==== 4-bit interface ===========
+ LCD_send_whole_byte( (1 << 5) | (1 << 3));         // 4-bit interface, 2 lines, 5x8 dots per symbol
+ _delay_us(100);
+ LCD_send_whole_byte(1 << 3);                       // display OFF
+ _delay_us(100);
+ LCD_send_whole_byte(1);                            // display clear
+ _delay_ms(3);
+ LCD_send_whole_byte(0x06);                         // entry mode, increment, no display shift
+ _delay_us(100);
+ LCD_send_whole_byte(0x0F);                         // display ON, blinking cursor
+ _delay_ms(3);
+}
+
+// Display text (string)
+void LCD_text(char *str)
+{
+ while (*str) LCD_send_data(*str++);
+}
+
+// Display value of an integer variable
+void LCD_integer(uint16_t value)
+{
+ char buf1[7];
+ LCD_text(itoa(value, buf1, 10));                   // base 10 
+}
+
+// Clear display
+void LCD_clear(void)
+{
+ LCD_send_cmd(0x01);
+ _delay_ms(3);
+}
+
+// Move cursor to a desired position
+void LCD_gotoXY(uint8_t x, uint8_t y)
+{
+ uint8_t addr = 0;
+ if (y == 1) addr = x;                              // first row  (address 0x00 = 0)
+ if (y == 2) addr = x + 0x40;                       // second row (address 0x40 = 64)
+ if (y == 3) addr = x + 0x54;                       // third row  (address 0x54 = 84)
+ if (y == 4) addr = x + 0x14;                       // fourth row (address 0x14 = 20)
+ LCD_send_cmd((1 << 7) | addr);
+ _delay_us(40);
+}
---
+++ LCD_HD44780/main.c
@@ -0,0 +1,21 @@
+// ========================================================
+// (c)2022 SalaScience
+// Example: Using LCD library to display text and variables
+// ========================================================
+#include <avr/io.h>
+#include "lcd_lib.h"
+#include <util/delay.h>
+int main (void)
+{
+  LCD_init();             // LCD initialization
+  uint16_t voltage =5;
+  while (1)
+  {
+   LCD_gotoXY(0,1);            // go to position 0 in the first row, 
+   LCD_text("Voltage = ");     // displaying text
+   LCD_integer(voltage); 		  // displaying variables
+   LCD_text(" V   ");
+   _delay_ms(1000);
+  }
+  return 0;
+}
